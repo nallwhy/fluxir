03b7faf33c67a8e7a1f6914723ef1d13fe9c4850
json <json@momenti.tv>
2021-11-28 04:18:37 +0900

    Add tests for Fluxir.FunctionTest

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Fluxir.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -23,6 +24,12 @@ defmodule Fluxir.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp aliases() do
+    [
+      test: ["test --warnings-as-errors"],
     ]
   end
 end
