1cefe94bee3bb23350aa32aa26ad696888421169
json <json@momenti.tv>
2021-11-28 04:56:15 +0900

    Add to_flux/1 to Fluxir

--- lib/fluxir.ex
+++ lib/fluxir.ex
@@ -1,3 +1,13 @@
 defmodule Fluxir do
   defstruct functions: []
+
+  def to_flux(%__MODULE__{functions: functions}) do
+    functions
+    |> Enum.map(fn function ->
+      function.__struct__.to_flux(function)
+    end)
+    |> Enum.reverse()
+    |> Enum.join("\n|> ")
+    |> Kernel.<>("\n")
+  end
 end
--- test/fluxir_test.exs
+++ test/fluxir_test.exs
@@ -6,4 +6,16 @@ defmodule FluxirTest do
 
     assert fluxir.functions == []
   end
+
+  test "to_flux/1" do
+    test_function0 = Fluxir.TestFunction.new(key: 0)
+    test_function1 = Fluxir.TestFunction.new(key: 1)
+
+    assert """
+           test(key: 1)
+           |> test(key: 0)
+           """ ==
+             %Fluxir{functions: [test_function0, test_function1]}
+             |> Fluxir.to_flux()
+  end
 end
