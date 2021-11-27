71e3e619ac5df81908d1d7aac7f1e13ee219d918
json <json@momenti.tv>
2021-11-28 04:56:03 +0900

    Refactor Fluxir.TestFunction

--- test/fluxir/function_test.exs
+++ test/fluxir/function_test.exs
@@ -1,11 +1,2 @@
 defmodule Fluxir.FunctionTest do
-  defmodule Test do
-    @behaviour Fluxir.Function
-
-    @impl true
-    def new(_keyword), do: struct(Test, [])
-
-    @impl true
-    def to_flux(_struct), do: ""
-  end
 end
