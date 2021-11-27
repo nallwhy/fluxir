4ff491fc895c447bb5480e64fccee8466074971b
json <json@momenti.tv>
2021-11-28 03:44:37 +0900

    Cleanup Fluxir

--- lib/fluxir.ex
+++ lib/fluxir.ex
@@ -1,18 +1,2 @@
 defmodule Fluxir do
-  @moduledoc """
-  Documentation for `Fluxir`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Fluxir.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/fluxir_test.exs
+++ test/fluxir_test.exs
@@ -1,8 +1,2 @@
 defmodule FluxirTest do
-  use ExUnit.Case
-  doctest Fluxir
-
-  test "greets the world" do
-    assert Fluxir.hello() == :world
-  end
 end
