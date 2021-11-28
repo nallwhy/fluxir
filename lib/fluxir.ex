a543d3404b5f428db8a5ac348d5f1b9961f1892d
json <json@momenti.tv>
2021-11-28 13:24:50 +0900

    Add custom/2 to Fluxir

--- lib/fluxir.ex
+++ lib/fluxir.ex
@@ -15,10 +15,22 @@ defmodule Fluxir do
   end
 
   @doc """
-  Build [from()](https://docs.influxdata.com/flux/v0.x/stdlib/influxdata/influxdb/from/) function.
+  Append [from()](https://docs.influxdata.com/flux/v0.x/stdlib/influxdata/influxdb/from/) function to Fluxir functions.
   """
   def from(%__MODULE__{functions: functions} = fluxir \\ %__MODULE__{}, params) do
     new_functions = [Fluxir.Function.Influxdata.Influxdb.From.new(params) | functions]
+    %__MODULE__{fluxir | functions: new_functions}
+  end
+
+  @doc """
+  Append custom function to Fluxir functions.
+
+  iex> Fluxir.Custom("anyFunction(key: value)")
+  %Fluxir.Function.Custom{flux: "anyFunction(key: value)"}
+
+  """
+  def custom(%__MODULE__{functions: functions} = fluxir \\ %__MODULE__{}, flux) do
+    new_functions = [Fluxir.Function.Custom.new(flux) | functions]
     %__MODULE__{fluxir | functions: new_functions}
   end
 end
