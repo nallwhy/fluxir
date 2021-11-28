f4a0f6f96405445f80ea372d915518c3302db53d
json <json@momenti.tv>
2021-11-28 12:09:38 +0900

    Add from/2 to Fluxir

--- lib/fluxir.ex
+++ lib/fluxir.ex
@@ -1,6 +1,9 @@
 defmodule Fluxir do
   defstruct functions: []
 
+  @doc """
+  Convert Fluxir functions to Flux string.
+  """
   def to_flux(%__MODULE__{functions: functions}) do
     functions
     |> Enum.map(fn function ->
@@ -9,5 +12,13 @@ defmodule Fluxir do
     |> Enum.reverse()
     |> Enum.join("\n|> ")
     |> Kernel.<>("\n")
+  end
+
+  @doc """
+  Build [from()](https://docs.influxdata.com/flux/v0.x/stdlib/influxdata/influxdb/from/) function.
+  """
+  def from(%__MODULE__{functions: functions} = fluxir \\ %__MODULE__{}, params) do
+    new_functions = [Fluxir.Function.Influxdata.Influxdb.From.new(params) | functions]
+    %__MODULE__{fluxir | functions: new_functions}
   end
 end
